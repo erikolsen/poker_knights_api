7238f2dcf269ebd83cc580ae1f7287372d928ab9
Erik Olsen <erik.s.olsen@gmail.com>
2018-09-24 12:03:29 -0500

    Fix typo in hands controller

--- app/controllers/games/hands_controller.rb
+++ app/controllers/games/hands_controller.rb
@@ -1,32 +1,4 @@
 module Games
   class HandsController < ActionController::API
-    #def show
-      #puts params
-      #game = Game.find_by(slug: params[:id]) || GameCreator.new.game
-
-      #cards = game.cards
-      #knights = game.position.last
-      #white = game.white
-      #black = game.black
-
-      #render json: {white: {hand: white},
-                    #black: {hand: black},
-                    #knights: knights,
-                    #cards: cards }
-    #end
-
-    #def move
-      #game = Game.find(params[:game_id])
-      #position = params[:game][:position]
-      #move = params[:game][:move]
-      #game.add_move(position, move)
-      #if game.save
-        #MovesChannel.broadcast_to game, {move: game.position.last}
-        #render json: {success: true }
-      #else
-        #render json: {failure: true }
-      #end
-    #end
-
-  #end
+  end
 end
